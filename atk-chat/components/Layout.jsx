05d8e7804f41f5cf1d9aa7bc33b7785ff99f73c7
Adam Khodier <khodieradam1297@gmail.com>
2023-02-08 16:00:58 -0500

    google auth working

--- atk-chat/components/Layout.jsx
+++ atk-chat/components/Layout.jsx
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 export default function Layout({ children }){
     return(
-        <div className="mx-6 max-w-2xl md:mx-auto bg-red-50 font-poppins">
+        <div className="mx-6 max-w-2xl md:mx-auto">
             <Nav/>
             <main>{children}</main>
         </div>
--- atk-chat/components/Nav.jsx
+++ atk-chat/components/Nav.jsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
+
 export default function Nav() {
     return(
         <div>
-            <h1>Nav</h1>
+            <nav className="flex justify-between items-center py-10">
+                <Link href="/">
+                    <button className="text-lg font-medium">atk-chat</button>
+                </Link>
+                <ul className="flex items-center gap-10">
+                    <Link href={'/auth/login'}>
+                       <p className="py-2 px-4 ext-sm bg-cyan-500 rounded-lg font-medium ml-8">Join Now</p>
+                    </Link>
+                </ul>
+            </nav>
         </div>
     )
 }
