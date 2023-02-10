0a459bb6f9c40cde99d0519ad3c6207b99986197
Adam Khodier <khodieradam1297@gmail.com>
2023-02-10 10:18:36 -0500

    trying to deploy

--- atk-chat/components/Layout.jsx
+++ atk-chat/components/Layout.jsx
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 export default function Layout({ children }){
     return(
-        <div className="mx-6 max-w-2xl md:mx-auto">
+        <div className="mx-6 max-w-2xl md:mx-auto font-poppins">
             <Nav/>
             <main>{children}</main>
         </div>
--- atk-chat/pages/post.js
+++ atk-chat/pages/post.js
@@ -1,21 +1,21 @@
-import { auth, db } from '../utils/firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+// import { auth, db } from '../utils/firebase';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
 
-export default function Post(){
-    return(
-        <div className='my-20 p-12 shadow-lg shadow-cyan-500 rounded-lg max-w-md mx-auto '>
-            <form>
-                <h1 className='text-2xl font-bold'>Create a new post!</h1>
-                <div className='py-2'>
-                    <h3 className='text-lg font-medium py-2'>Description</h3>
-                    <textarea className='bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm'></textarea>
-                    <p className=''>0/300</p>
-                    <button className='w-full bg-cyan-500 text-white font-medium p-2 my-2 rounded-lg text-sm'>Submit</button>
-                </div>
-            </form>
-        </div>
-    )
-}
+// export default function Post(){
+//     return(
+//         <div className='my-20 p-12 shadow-lg shadow-cyan-500 rounded-lg max-w-md mx-auto '>
+//             <form>
+//                 <h1 className='text-2xl font-bold'>Create a new post!</h1>
+//                 <div className='py-2'>
+//                     <h3 className='text-lg font-medium py-2'>Description</h3>
+//                     <textarea className='bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm'></textarea>
+//                     <p className=''>0/300</p>
+//                     <button className='w-full bg-cyan-500 text-white font-medium p-2 my-2 rounded-lg text-sm'>Submit</button>
+//                 </div>
+//             </form>
+//         </div>
+//     )
+// }
 
