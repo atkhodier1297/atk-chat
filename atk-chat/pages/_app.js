3fa224cb03f6c05761fb4eabf2dd035c8f64fdbc
Adam Khodier <khodieradam1297@gmail.com>
2023-02-10 15:34:00 -0500

    fb collection works

--- atk-chat/pages/_app.js
+++ atk-chat/pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css'
 import Layout from '@/components/Layout'
+import { ToastContainer } from 'react-toastify'
 
 export default function App({ Component, pageProps }) {
   return(
     <Layout>
+      <ToastContainer Limit={1}/>
       <Component {...pageProps} />
     </Layout>
   )
--- atk-chat/pages/post.js
+++ atk-chat/pages/post.js
@@ -1,21 +1,62 @@
-// import { auth, db } from '../utils/firebase';
-// import { useAuthState } from 'react-firebase-hooks/auth';
-// import { useRouter } from "next/router";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import {
+    addDoc,
+    collection,
+    serverTimestamp,
+  } from "firebase/firestore";
+import { auth, db } from "@/utils/firebase";
 
-// export default function Post(){
-//     return(
-//         <div className='my-20 p-12 shadow-lg shadow-cyan-500 rounded-lg max-w-md mx-auto '>
-//             <form>
-//                 <h1 className='text-2xl font-bold'>Create a new post!</h1>
-//                 <div className='py-2'>
-//                     <h3 className='text-lg font-medium py-2'>Description</h3>
-//                     <textarea className='bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm'></textarea>
-//                     <p className=''>0/300</p>
-//                     <button className='w-full bg-cyan-500 text-white font-medium p-2 my-2 rounded-lg text-sm'>Submit</button>
-//                 </div>
-//             </form>
-//         </div>
-//     )
-// }
 
+
+export default function Post() {
+  //Form state
+  const [post, setPost] = useState({ description: "" });
+  const [user, loading] = useAuthState(auth)
+
+  //Submit Post
+  const submitPost = async (e) => {
+    e.preventDefault();
+    const collectionRef = collection(db, 'posts');
+    await addDoc(collectionRef, {
+        ...post,
+        timestamp: serverTimestamp(),
+        user: user.uid,
+        avatar: user.photoURL,
+        username: user.displayName
+    })
+  };
+
+  return (
+    <div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
+      <form onSubmit={submitPost}>
+        <h1 className="text-2xl font-bold">
+          {post.hasOwnProperty("id") ? "Edit your post" : "Create a new post"}
+        </h1>
+        <div className="py-2">
+          <h3 className="text-lg font-medium py-2">Description</h3>
+          <textarea
+            value={post.description}
+            onChange={(e) => setPost({ ...post, description: e.target.value })}
+            className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm"
+          >
+          </textarea>
+          <p
+            className={`text-cyan-500 font-medium text-sm ${
+              post.description.length > 100 ? "text-red-600" : ""
+            }`}
+          >
+            {post.description.length}/100
+          </p>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-cyan-500 text-white font-medium p-2 my-2 rounded-lg text-sm"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
